0e3374cc8b18dc9c21f5d1979f5243f7ded8e0e7
Samuel Abati <otl.acteast@gmail.com>
2021-04-14 19:24:19 +0100

    First push to github

---
+++ budget_app.py
@@ -0,0 +1,47 @@
+class Budget:
+	def __init__(self, category, availableBalance = 0):
+		self.category  = category
+		self.availableBalance = availableBalance
+
+	def depositFunds(self, amount):
+		''' Adds the amount provided to the residual balance of the budget'''
+		self.availableBalance += amount
+		return self.availableBalance
+
+	def withdrawFunds(self, amount):
+		''' Withdraws the amount specified from the residual balance of the budget'''
+		if (amount <= self.availableBalance):
+			self.availableBalance -= amount
+			return self.availableBalance
+		else:
+			print(f"You just tried to withdraw more than the available funds in the {self.category} budget")
+
+	def balance(self):
+		''' Prints the available balance of the budget category upon which it is called upon'''
+		print(f"The available balance in the {self.category} budget is {self.availableBalance} naira")
+
+	def transferFunds(self, categoryToTransferTo, amount):
+		self.availableBalance -= amount
+		categoryToTransferTo.availableBalance += amount
+		print(f"{amount} naira has been transferred from {self.category} budget to {categoryToTransferTo.category} budget")
+		
+
+	
+
+
+		
+food = Budget("food", 100056)
+food.withdrawFunds(56789)
+food.balance()
+
+bags = Budget("bags")
+bags.balance()
+
+food.transferFunds(bags, 100)
+bags.balance()
+food.balance()
+
+music = Budget("music", 123456)
+music.withdrawFunds(70980)
+music.depositFunds(6787)
+music.balance()
